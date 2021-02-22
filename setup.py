95da9a021bacbe491a86022c2eb72f4d5f91e72b
Luke Parry <dev@lukeparry.uk>
2021-02-22 20:19:35 +0000

    Update version for small fixes

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Natural Language :: English',
         'Topic :: Scientific/Engineering'],
     license="",
